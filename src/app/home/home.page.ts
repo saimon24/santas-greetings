2fb85c66cfcf953f8728b0a4ecbd4c61a4140752
Simon Grimm <saimon@devdactic.com>
2022-12-15 15:43:20 +0100

    Snowflakes and basic stuff.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import Snowflakes from 'magic-snowflakes';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  greetingsForm = this.fb.group({
+export class HomePage implements AfterViewInit {
+  greetingsForm = this.fb.nonNullable.group({
     greetings: ['', Validators.required],
+    email: ['', [Validators.email, Validators.required]],
   });
+  snowflakes!: Snowflakes;
 
   constructor(private fb: FormBuilder) {}
 
-  submit() {}
+  ngAfterViewInit() {
+    this.startSnow();
+  }
+
+  startSnow() {
+    this.snowflakes = new Snowflakes({
+      color: '#fff',
+    });
+  }
+
+  submit() {
+    const { greetings } = this.greetingsForm.getRawValue();
+  }
+
+  selectImage() {}
 }
