fc308b1a0412b55d29ffb5c5cf06bfd230d457b0
Simon Grimm <saimon@devdactic.com>
2022-12-15 15:28:40 +0100

    Basic UI.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  greetingsForm = this.fb.group({
+    greetings: ['', Validators.required],
+  });
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
+  submit() {}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://ihoucpfmipoxzohnjofs.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlob3VjcGZtaXBveHpvaG5qb2ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzExMTA5NjMsImV4cCI6MTk4NjY4Njk2M30.a7SSM4XIlMu25FF5jdFa-wx-P7yCKHMFJNZxAZDpzuk',
 };
 
 /*
