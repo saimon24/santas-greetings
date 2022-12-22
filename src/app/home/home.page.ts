3e2f6f2265e97ee9ec45ed5b2f01ca69fba47fe2
Simon Grimm <saimon@devdactic.com>
2022-12-22 15:36:13 +0100

    Upload working.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,10 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { LoadingController, ToastController } from '@ionic/angular';
 import Snowflakes from 'magic-snowflakes';
-import { SupabaseService } from '../services/supabase.service';
+import { Observable } from 'rxjs';
+import { Greeting, SupabaseService } from '../services/supabase.service';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +12,22 @@ import { SupabaseService } from '../services/supabase.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements AfterViewInit {
+  @ViewChild('video') captureElement!: ElementRef;
+  mediaRecorder!: MediaRecorder;
+
   greetingsForm = this.fb.nonNullable.group({
     greetings: ['', Validators.required],
     email: ['', [Validators.email, Validators.required]],
   });
   snowflakes!: Snowflakes;
-
+  isRecording = false;
+  videoBlob?: Blob;
+  greetings = this.supabaseService.getGreetings();
   constructor(
     private fb: FormBuilder,
-    private supabaseService: SupabaseService
+    private supabaseService: SupabaseService,
+    private loadingController: LoadingController,
+    private toastController: ToastController
   ) {}
 
   ngAfterViewInit() {
@@ -31,22 +40,82 @@ export class HomePage implements AfterViewInit {
     });
   }
 
-  submit() {
-    const { greetings } = this.greetingsForm.getRawValue();
+  async submit() {
+    const { greetings, email } = this.greetingsForm.getRawValue();
+    const greeting: Greeting = {
+      text: greetings,
+      email,
+      videoBuffer: this.videoBlob,
+    };
+    const loading = await this.loadingController.create({
+      message: 'Uploading to Santa...',
+      duration: 2000,
+      spinner: 'bubbles',
+    });
+    await loading.present();
+
+    const result = await this.supabaseService.addGreeting(greeting);
+    loading.dismiss();
+
+    const toast = await this.toastController.create({
+      message: 'Thanks for your greetings!',
+      duration: 2000,
+    });
+    toast.present();
   }
 
   async captureVideo() {
-    const video = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: true,
-      source: CameraSource.Photos,
-      resultType: CameraResultType.Uri,
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'user',
+      },
+      audio: true,
     });
-    console.log(
-      '🚀 ~ file: home.page.ts:41 ~ HomePage ~ captureVideo ~ video',
-      video
-    );
-    const result = await this.supabaseService.uploadVideo(video);
-    console.log('IM DONE: ', result);
+
+    // Show the stream inside our video object
+    this.captureElement.nativeElement.srcObject = stream;
+
+    var options = { mimeType: 'video/webm' };
+    this.mediaRecorder = new MediaRecorder(stream, options);
+    let chunks: any[] = [];
+
+    // Store the video on stop
+    this.mediaRecorder.onstop = async (event) => {
+      this.videoBlob = new Blob(chunks, { type: 'video/webm' });
+      console.log('got video: ', this.videoBlob);
+      // const result = await this.supabaseService.uploadVideo(videoBuffer);
+    };
+
+    // Store chunks of recorded video
+    this.mediaRecorder.ondataavailable = (event) => {
+      if (event.data && event.data.size > 0) {
+        chunks.push(event.data);
+      }
+    };
+
+    // Start recording wth chunks of data
+    this.mediaRecorder.start(100);
+    this.isRecording = true;
+  }
+
+  stopRecording() {
+    this.mediaRecorder.stop();
+    this.captureElement.nativeElement.srcObject = null;
+    this.isRecording = false;
+  }
+
+  async capturePhoto() {
+    // const video = await Camera.getPhoto({
+    //   quality: 90,
+    //   allowEditing: true,
+    //   source: CameraSource.Photos,
+    //   resultType: CameraResultType.Uri,
+    // });
+    // console.log(
+    //   '🚀 ~ file: home.page.ts:41 ~ HomePage ~ captureVideo ~ video',
+    //   video
+    // );
+    // const result = await this.supabaseService.uploadVideo(video);
+    // console.log('IM DONE: ', result);
   }
 }
--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -2,45 +2,101 @@ import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
 import { Photo } from '@capacitor/camera';
+import { BehaviorSubject } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 
 export interface Greeting {
   id?: number;
   text: string;
   email: string;
-  video: string;
+  videoFile?: any;
+  videoBuffer?: Blob;
+  createdAt?: string;
 }
 export const TABLE_GREETINGS = 'greetings';
+export const BUCKET_VIDEOS = 'videos';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SupabaseService {
+  private greetings: BehaviorSubject<Greeting[]> = new BehaviorSubject<
+    Greeting[]
+  >([]);
+
   private supabase: SupabaseClient;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     this.supabase = createClient(
       environment.supabaseUrl,
       environment.supabaseKey
     );
+
+    this.loadGreetings();
   }
 
-  async uploadVideo(video: Photo) {
-    const blob = await fetch(video.webPath!).then((r) => r.blob());
+  async loadGreetings(offset = 0) {
+    const result = await this.supabase
+      .from(TABLE_GREETINGS)
+      .select('*')
+      .range(offset, offset + 5)
+      .order('createdAt');
+
+    let videos = result.data || [];
+    videos = videos.map((video) => {
+      video.videoFile = this.downloadVideo(video.video);
+      return video;
+    });
+    console.log('MY files: ', videos);
+
+    this.greetings.next(videos);
+  }
+
+  async downloadVideo(filename: string) {
+    const { data, error } = await this.supabase.storage
+      .from(BUCKET_VIDEOS)
+      .download(`${filename}`);
+
+    console.log(data);
+
+    const result = URL.createObjectURL(data!);
+    console.log('return data: ', result);
+    return this.sanitizer.bypassSecurityTrustUrl(result);
+    result;
+  }
+
+  async addGreeting(greeting: Greeting) {
+    const uploadResult = await this.uploadVideo(
+      greeting.videoBuffer!,
+      greeting.email
+    );
+
+    return this.supabase.from(TABLE_GREETINGS).insert([
+      {
+        text: greeting.text,
+        email: greeting.email,
+        video: uploadResult?.path,
+      },
+    ]);
+  }
+
+  async uploadVideo(blob: Blob, email: string) {
     const file = new File([blob], 'myfile', {
       type: blob.type,
     });
 
     const time = new Date().getTime();
-    const bucketName = 'videos';
-    const fileName = `${Math.random() * 1000}-${time}.webp`;
+    const fileName = `${email}-${Math.random() * 100}-${time}.webm`;
 
     // Upload the file to Supabase
     const { data, error } = await this.supabase.storage
-      .from(bucketName)
+      .from(BUCKET_VIDEOS)
       .upload(fileName, file);
 
     return data;
   }
 
-  addGreeting(greeting: Greeting) {}
+  getGreetings() {
+    return this.greetings.asObservable();
+  }
 }
